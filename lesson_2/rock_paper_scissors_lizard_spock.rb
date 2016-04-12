b3bf183dfc33b45cdbd6ba0fd6164c3546046698
Julia McAleenan <juliamcaleenan@gmail.com>
2016-04-12 12:38:05 +0100

    improvements to 'Rock, Paper, Scissors, Lizard, Spock' game

--- lesson_2/rock_paper_scissors_lizard_spock.rb
+++ lesson_2/rock_paper_scissors_lizard_spock.rb
@@ -8,8 +8,16 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def press_enter_to_continue
+  loop do
+    prompt "-----------------------------------------------------"
+    prompt "Press enter to continue..."
+    break if gets.include?("\n")
+  end
+end
+
 def display_valid_choices
-  VALID_CHOICES.each { |k, v| prompt "For #{v}, enter '#{k}'" }
+  VALID_CHOICES.each { |key, choice| prompt "For #{choice}, enter '#{key}'" }
 end
 
 def win?(first, second)
@@ -29,42 +37,56 @@ end
 loop do
   player_score = 0
   computer_score = 0
+
+  system('clear') || system('cls')
+  prompt "Welcome to Rock, Paper, Scissors, Lizard, Spock."
+  prompt "The winner is the first to reach 5 points. Good luck!"
+  press_enter_to_continue
+
   loop do
     system('clear') || system('cls')
-    prompt "Welcome to Rock, Paper, Scissors, Lizard, Spock."
-    prompt "The winner is the first to reach 5 points. Good luck!"
+    prompt "Current scores: You: #{player_score}; computer: #{computer_score}"
     prompt "-----------------------------------------------------"
-    prompt "Current scores: You: #{player_score}; computer: #{computer_score}\n\n"
 
     player_choice = ''
     prompt "Make your choice:"
     display_valid_choices
     loop do
-      player_choice = gets.chomp
+      player_choice = gets.chomp.downcase
       break if VALID_CHOICES.keys.include?(player_choice)
       prompt "Please enter a valid choice."
     end
 
     computer_choice = VALID_CHOICES.keys.sample
 
-    prompt "You chose: #{VALID_CHOICES[player_choice]}; computer chose: #{VALID_CHOICES[computer_choice]}"
+    prompt "You chose: #{VALID_CHOICES[player_choice]}; "\
+           "computer chose: #{VALID_CHOICES[computer_choice]}"
 
     display_result(player_choice, computer_choice)
-    sleep 2
 
     player_score += 1 if win?(player_choice, computer_choice)
     computer_score += 1 if win?(computer_choice, player_choice)
     break if player_score == WINNING_SCORE || computer_score == WINNING_SCORE
+
+    press_enter_to_continue
   end
 
   prompt "-----------------------------------------------------"
-  prompt "You have #{WINNING_SCORE} points and have won!" if player_score == WINNING_SCORE
-  prompt "Computer has #{WINNING_SCORE} points and has won!" if computer_score == WINNING_SCORE
-
+  if player_score == WINNING_SCORE
+    prompt "You have #{WINNING_SCORE} points and have won!"
+  elsif computer_score == WINNING_SCORE
+    prompt "Computer has #{WINNING_SCORE} points and has won!"
+  end
   prompt "-----------------------------------------------------"
-  prompt "Do you want to play again?"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+
+  answer = ''
+  prompt "Do you want to play again (y/n)?"
+  loop do
+    answer = gets.chomp.downcase
+    break if %w(y n yes no).include?(answer)
+    prompt "Enter 'y' to play again or 'n' to quit."
+  end
+  break unless answer == 'y' || answer == 'yes'
 end
 
 prompt "Thank you for playing. Good bye!"
